5a1136fe653883c10b5664585655b794a1287226
gaoyong11 <gaoyongs72@163.com>
2019-09-04 22:42:01 +0800

    Laravel中基于Swoole实现异步任务队列-路由及配置编写

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Rotue::get('/task/test', function () {
+   $task = new \App\Jobs\TestTask('测试异步任务');
+   // 异步投递任务,触发调用任务类的handle方法
+   $success = \Hhxsv5\LaravelS\Swoole\Task\Task::deliver($task);
+   var_dump($success);
+});
