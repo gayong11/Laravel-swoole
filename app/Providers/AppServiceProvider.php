ba36ad933f8e76fe41b6c429580f8ae8f4c3b3fa
gaoyong11 <gaoyongs72@163.com>
2019-09-06 10:31:34 +0800

    应用级别系统事件处理

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +16,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Event::listen('laravels.received_request', function (Request $request, $app) {
+            $request->query->set('get_key', 'swoole-get-param');
+            $request->request->set('post_key', 'swoole-post-param');
+        });
+
+        Event::listen('laravels.generated_response', function (Request $request, Response $response, $app) {
+            $response->header->set('header-key', 'swoole-header');
+        });
     }
 
     /**
